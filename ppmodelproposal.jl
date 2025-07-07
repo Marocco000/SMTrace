7459f79ce27d5e21482531229e73517f09c16dc2
Mara Coman <s.coman-1@student.tudelft.nl>
2025-07-07 19:20:04 +0200

    Cleanup

--- ppmodelproposal.jl
+++
@@ -1,8 +0,0 @@
-@gen function smt_proposal()
-	A ~ normal(0, epsilon)
-	intercept ~ normal(0, epsilon)
-	slope ~ normal(0, epsilon)
-	B ~ normal(1, epsilon)
-	noise ~ normal(0.019061625176369425, epsilon)
-	C ~ normal(0.9, epsilon)
-end
